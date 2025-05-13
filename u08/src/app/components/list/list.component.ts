72230e0f39f5c2c37418832bf965f54c16d2c676
scvhea <svea.holm03@gmail.com>
2025-05-13 10:39:55 +0200

    formulär lägg till film html+ metod för att hantera formuläret

--- u08/src/app/components/list/list.component.ts
+++ u08/src/app/components/list/list.component.ts
@@ -3,16 +3,24 @@ import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { ConnectService } from '../../services/connect.service';
 import { Movie } from '../../models/movie.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-list',
-  imports: [CommonModule],
+  imports: [CommonModule,FormsModule],
   standalone: true,
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
 export class ListComponent implements OnInit{
   movies: Movie[] = [];
+
+  newMovie: Movie = {
+    title: '',
+    created: new Date(), 
+    genre: '',
+    director: ''
+  };
 
   constructor(private service: ConnectService, private router: Router) {}
   
@@ -30,5 +38,18 @@ export class ListComponent implements OnInit{
 
   goToEdit(id: string): void {
     this.router.navigate(['/movie', id]);
+  }
+  addMovie(): void {
+    this.service.create(this.newMovie).subscribe({
+      next: () => {
+        this.loadMovies();
+        this.newMovie = {
+          title: '',
+          created: new Date(),
+          genre: '',
+          director: ''
+        };
+      },
+    });
   }
 }
