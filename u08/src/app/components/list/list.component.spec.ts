c8abe14460bcfa9caae82e534bcf97e0b529d6a2
scvhea <svea.holm03@gmail.com>
2025-05-15 22:11:28 +0200

    imports tester

--- u08/src/app/components/list/list.component.spec.ts
+++ u08/src/app/components/list/list.component.spec.ts
@@ -1,10 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ListComponent } from './list.component';
+import { ConnectService } from '../../services/connect.service';
+import { Router } from '@angular/router';
+import { of } from 'rxjs';
+import { Movie } from '../../models/movie.model';
+
 
 describe('ListComponent', () => {
   let component: ListComponent;
   let fixture: ComponentFixture<ListComponent>;
+  let mockService: jasmine.SpyObj<ConnectService>;
+  let mockRouter: jasmine.SpyObj<Router>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
